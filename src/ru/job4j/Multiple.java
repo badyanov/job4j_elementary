a838cfccc782337d6fbcf21ff0b0587d4a002ab6
badyanov <d.badyanov@outlook.com>
2020-05-08 21:15:00 +0300

    3.3. Разбор первой программы.[#285641]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        // таблица умножения на 1
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("1 * " + i + " = " + i);
+        }
+    }
+}
