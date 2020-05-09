a9334c7c0fc247ec35aaa0ddfe84380e84e34de4
badyanov <d.badyanov@outlook.com>
2020-05-09 14:40:56 +0300

    5. Конвертер валюты[#285621]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,33 @@
+package ru.job4j.converter;
+
+public class Converter {
+    static int EUR = 70;
+    static int USD = 60;
+
+    public static int rubleToEuro(int value) {
+        return value / EUR;
+    }
+    public static int euroToRuble(int value) {
+        return value * EUR;
+    }
+    public static int rubleToDollar(int value) {
+        return value / USD;
+    }
+    public static int dollarToRuble(int value) {
+        return value * USD;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+
+        int dollar = Converter.rubleToDollar(115000);
+        System.out.println("115000 rubles are " + dollar + " dollars.");
+
+        int rublesFromEuro = Converter.euroToRuble(500);
+        System.out.println("500 euro are " + rublesFromEuro + " rubles.");
+
+        int rublesFromDollars = Converter.dollarToRuble(3000);
+        System.out.println("3000 dollars are " + rublesFromDollars + " rubles.");
+    }
+}
