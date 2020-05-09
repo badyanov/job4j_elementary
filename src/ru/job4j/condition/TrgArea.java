fa5cd8a64e55e435ae31bd5b487b09d53ee0c531
badyanov <d.badyanov@outlook.com>
2020-05-09 18:42:19 +0300

    9. Площадь треугольника.[#285628]

---
+++ src/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+
+    public static double area(double a, double b, double c) {
+        // Площадь треугольника по формуле Герона
+        double p = (a+b+c)/2;
+        double rsl = Math.sqrt(p * (p-a)*(p-b)*(p-c));
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double rsl1 = TrgArea.area(2, 2, 2);
+        System.out.println("area (2, 2, 2) = " + rsl1);
+
+        double rsl2 = TrgArea.area(3, 4, 5);
+        System.out.println("area (3, 4, 5) = " + rsl2);
+    }
+}
