831887189941e581c77ae371717fd72bc5fda3ae
badyanov <d.badyanov@outlook.com>
2020-05-11 01:27:48 +0300

    4.1.2. Глупый бот.[#285582]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+
+        if (question.equals("Привет, Бот.")) {
+            rsl = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
+            rsl = "До скорой встречи.";
+        }
+
+        return rsl;
+    }
+
+    public static void printAnswer(String question) {
+        System.out.println(answer(question));
+    }
+
+    public static void main(String[] args) {
+        printAnswer("Привет, Бот.");
+        printAnswer("Пока.");
+        printAnswer("Как тебя зовут?");
+    }
+
+}
