75d850dc479efb2e24fc8a7cde2ff1b2572e6b45
badyanov <d.badyanov@outlook.com>
2020-05-14 00:16:11 +0300

    5.6. Простые числа.[#285672]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+// Метод должен вычислять количество простых чисел от 1 до X включительно.
+
+public class PrimeNumber {
+
+    public int calc(int finish) {
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
