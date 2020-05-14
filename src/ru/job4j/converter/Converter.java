92ced62b5305960dc2b0b417ba91c824adbfbf06
badyanov <d.badyanov@outlook.com>
2020-05-15 01:00:54 +0300

    5.8. JavaDoc. Code convention[#285682] - добавлена документация JavaDoc

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,22 +1,63 @@
 package ru.job4j.converter;
 
+/**
+ * Класс предназначен для конвертации валюты (Доллар, Евро) в рубли и обратно
+ * Курс доллара и евро устанавливаются вручную в константах класса
+ *
+ * @author Дмитрий Бадьянов (d.badyanov@outlook.com)
+ */
 public class Converter {
+    /**
+     * Курс Евро
+     */
     final static int EUR = 70;
+    /**
+     * Курс доллара
+     */
     final static int USD = 60;
 
+    /**
+     * Конвертация Рубль - Евро
+     * @param value - Количество в рублях
+     * @return количество в Евро (округление до целого)
+     */
     public static int rubleToEuro(int value) {
         return value / EUR;
     }
+
+    /**
+     * Конвертация Евро - Рубль
+     * @param value - Количество в Евро
+     * @return количество в рублях (округление до целого)
+     */
     public static int euroToRuble(int value) {
         return value * EUR;
     }
+
+    /**
+     * Конвертация Рубль - Доллар
+     * @param value - Количество в рублях
+     * @return количество в Долларах (округление до целого)
+     */
     public static int rubleToDollar(int value) {
         return value / USD;
     }
+
+    /**
+     * Конвертация Доллар - Рубль
+     * @param value - Количество в Долларах
+     * @return количество в рублях (округление до целого)
+     */
     public static int dollarToRuble(int value) {
         return value * USD;
     }
 
+    /**
+     * Метод используется для ручного тестирования корректности конвертации валют
+     * и выводит результат тестирования в консоль
+     *
+     * @param args - не используются
+     */
     public static void main(String[] args) {
         int in = 140;
         int expected = 2;
