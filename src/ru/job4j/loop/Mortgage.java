2b3229360f75b09b1d5c2ab8ed89736a9d5e5331
badyanov <d.badyanov@outlook.com>
2020-05-15 00:21:07 +0300

    5.7. Ипотека.[#285673]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+// посчитать количество лет, необходимых для погашения кредита.
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double amountDue = amount;
+
+        while (amountDue >= 0) {
+            amountDue += amountDue * (percent / 100) - salary;
+            year++;
+        }
+
+        return year;
+    }
+}
