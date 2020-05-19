5c76d5931fcb5fd1a19d0cf6b0110ec9c4f17a98
badyanov <d.badyanov@outlook.com>
2020-05-19 09:21:06 +0300

    6.6.1. Двухмерный массив. Вывод на консоль.[#285616]

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * 6.6.1. Двухмерный массив. Вывод на консоль.[#285616]
+ */
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] table = new int[3][3];
+
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                // К каждому значению во 2 строке прибавляется 3; в 3 строке - 6
+                table[y][x] = (x + 1) + (y * 3);
+            }
+        }
+
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                System.out.print(table[y][x] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
