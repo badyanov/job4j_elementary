45364b2d559f2e91d2eb39e3a515931fe5e622ee
badyanov <d.badyanov@outlook.com>
2020-05-19 08:17:44 +0300

    6.2.3. Перевернуть массив.[#285613] - оптимизация алгоритма

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -2,14 +2,11 @@ package ru.job4j.array;
 
 public class Turn {
     public int[] back(int[] array) {
+
         int lastIndex = array.length - 1;
 
-        for (int i = 0; i <= lastIndex; i++) {
-            // когда индекс начального элемента становится больше индекса конечного, выходим из цикла
-            if (i > lastIndex - i) {
-                break;
-            }
-
+        // Цикл доходит до середины массива
+        for (int i = 0; i <= lastIndex / 2; i++) {
             int temp = array[i];
             array[i] = array[lastIndex - i];
             array[lastIndex - i] = temp;
