6d28819fd1545e26a3afeeabc7a7b10f4a4264f8
badyanov <d.badyanov@outlook.com>
2020-06-15 08:29:26 +0300

    5.9.1. Генерация циклов в IDEA - iter, itar. [#285678]

---
+++ src/ru/job4j/live/Loop.java
@@ -0,0 +1,12 @@
+package ru.job4j.live;
+
+/**
+ * 5.9.1. Генерация циклов в IDEA - iter, itar. [#285678]
+ */
+public class Loop {
+    public static void main(String[] args) { // psvm
+        for (int i = 0; i <= 10; i++) { //fori
+            System.out.println(i); //sout
+        }
+    }
+}
